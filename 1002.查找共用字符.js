45b1653b244b413fe57f97c01f14371f29e20e17
xiechengyu <chengyu.xie@141data.com>
2023-03-22 11:47:03 +0800

    20230322

--- 1002.查找共用字符.js
+++ 1002.查找共用字符.js
@@ -2,6 +2,19 @@
  * @lc app=leetcode.cn id=1002 lang=javascript
  *
  * [1002] 查找共用字符
+ * 
+ * 给你一个字符串数组 words ，请你找出所有在 words 的每个字符串中都出现的共用字符（ 包括重复字符），并以数组形式返回。你可以按 任意顺序 返回答案。
+ 
+
+示例 1：
+
+输入：words = ["bella","label","roller"]
+输出：["e","l","l"]
+示例 2：
+
+输入：words = ["cool","lock","cook"]
+输出：["c","o"]
+ 
  */
 
 // @lc code=start
--- 917.仅仅反转字母.js
+++ 917.仅仅反转字母.js
@@ -2,6 +2,28 @@
  * @lc app=leetcode.cn id=917 lang=javascript
  *
  * [917] 仅仅反转字母
+ * 
+ * 给你一个字符串 s ，根据下述规则反转字符串：
+
+所有非英文字母保留在原有位置。
+所有英文字母（小写或大写）位置反转。
+返回反转后的 s 。
+
+ 
+
+示例 1：
+
+输入：s = "ab-cd"
+输出："dc-ba"
+示例 2：
+
+输入：s = "a-bC-dEf-ghIj"
+输出："j-Ih-gfE-dCba"
+示例 3：
+
+输入：s = "Test1ng-Leet=code-Q!"
+输出："Qedo1ct-eeLg=ntse-T!"
+ 
  */
 
 // @lc code=start
